c0daa37bc667d64c618ac9b46b81ceb69e5f0066
Josh Nussbaum <joshnuss@gmail.com>
2018-09-29 06:26:03 -0400

    Adds basic server

---
+++ index.js
@@ -0,0 +1,14 @@
+import { ApolloServer } from 'apollo-server';
+import { version } from './package.json';
+
+const typeDefs = [];
+
+const server = new ApolloServer({
+  typeDefs
+});
+
+server
+  .listen()
+  .then(({url}) => {
+    console.log(`CQL v${version} started at ${url}`);
+  });
