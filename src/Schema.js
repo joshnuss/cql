94a22da40027c685a0cc369672903c62baae99ff
Josh Nussbaum <joshnuss@gmail.com>
2018-09-29 07:45:30 -0400

    Adds basic schema

--- src/Schema.js
+++ src/Schema.js
@@ -1,9 +1,65 @@
-import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
+import {
+  GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLList,
+} from 'graphql';
+import fs from 'fs';
+import path from 'path';
+import { camelize } from 'inflected';
+
+const schemaPath = path.resolve(__dirname, '../example/.schema.json');
+const schemaData = fs.readFileSync(schemaPath, 'utf-8');
+const schema = JSON.parse(schemaData);
+
+const objectTypes = {};
+const typeMap = {
+  String: GraphQLString,
+};
+
+function typeForField(type) {
+  if (typeof (type) === 'object') {
+    if (type.type === 'Array') {
+      return {
+        type: new GraphQLList(typeForField(type.of).type),
+      };
+    }
+  }
+
+  const mapped = typeMap[type] || objectTypes[type];
+
+  return { type: mapped };
+}
+
+Object.keys(schema).forEach(name => {
+  const object = schema[name];
+
+  if (object.type === 'Object') {
+    objectTypes[name] = new GraphQLObjectType({
+      name: camelize(name),
+      fields: () => {
+        const fields = {};
+
+        Object.keys(object.fields).forEach(fieldName => {
+          fields[fieldName] = typeForField(object.fields[fieldName]);
+        });
+
+        return fields;
+      },
+    });
+  }
+});
 
 export default new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'Query',
     fields: {
+      teams: {
+        type: new GraphQLList(objectTypes.team),
+        resolve: () => {
+          const filePath = path.resolve(__dirname, '../example/teams.json');
+          const data = fs.readFileSync(filePath, 'utf-8');
+
+          return JSON.parse(data);
+        },
+      },
       hello: {
         type: GraphQLString,
         description: 'A simple type for getting started!',
